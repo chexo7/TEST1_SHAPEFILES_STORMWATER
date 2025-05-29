bdcae4ef0d9f48f92127f6d35f250995689b2965
chexo7 <sergio.acevedo.santic@gmail.com>
2025-05-29 17:18:41 -0400

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import MapComponent from '@/components/Map'; // Assuming @ is configured for src or app directory
-
-export default function HomePage() {
-  return (
-    <main>
-      <MapComponent googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY} />
-    </main>
-  );
-}
